f72d11a5f94accc09463e88e3fc9a6c9ad285bcc
kevin-jx <kevin-jx@jxinternet.com>
2018-06-01 14:51:39 +0800

    1.从数据库读取接口测试用例执行测试

--- src/main/java/com/apijava/apijava/controller/AccountController.java
+++ src/main/java/com/apijava/apijava/controller/AccountController.java
@@ -1,9 +1,11 @@
 package com.apijava.apijava.controller;
 
+import com.apijava.apijava.Utils.RestTem;
 import com.apijava.apijava.Utils.Tools;
 import com.apijava.apijava.domain.APIResult;
 import com.apijava.apijava.domain.TestResultSummary;
 import com.apijava.apijava.service.AccountService;
+import com.apijava.apijava.service.ApiInfoService;
 import com.apijava.apijava.service.ApiTestResultService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -22,13 +24,54 @@ public class AccountController {
 
     private AccountService accountService;
     private final ApiTestResultService apiTestResultService;
+    private final ApiInfoService apiInfoService;
+    private final RestTem restTem;
 
-    public AccountController(AccountService accountService, ApiTestResultService apiTestResultService) {
+    public AccountController(AccountService accountService, ApiTestResultService apiTestResultService, ApiInfoService apiInfoService, RestTem restTem) {
         this.accountService = accountService;
         this.apiTestResultService = apiTestResultService;
+        this.apiInfoService = apiInfoService;
+        this.restTem = restTem;
     }
 
-    @RequestMapping("/test")
+    @RequestMapping("sqlite")
+    public ModelAndView modelAndView() {
+
+        AtomicInteger success = new AtomicInteger();
+        AtomicInteger failed = new AtomicInteger();
+        AtomicInteger total = new AtomicInteger();
+        List<List<String>> resultList = new ArrayList<>();
+
+        resultList.clear();
+        //TODO 从sqlite中获取account的接口进行测试
+        String time = (new Tools()).getTimeName();
+        apiInfoService.findAllBySystemName("account-user").forEach(e -> restTem.excute(e));
+        //TODO 获取测试结果
+        apiTestResultService.getByCreateTimeAndSystemName(time, "account-user").forEach(e ->
+                resultList.add(Arrays.asList(e.getUrl(), e.getRemark(), e.getResponseBody() == null ? null: e.getResponseBody().toString(), String.valueOf(e.getStatus_code()), e.getVerification())));
+        List<APIResult> apiResults = new ArrayList<>();
+        List<TestResultSummary> testResultSummaries = new ArrayList<>();
+
+        resultList.forEach(list -> {
+            if (list.get(4).startsWith("Success")) {
+                success.addAndGet(1);
+            } else {
+                failed.addAndGet(1);
+            }
+            total.addAndGet(1);
+            APIResult apiResult = new APIResult(String.valueOf(total.get()), list.get(0), list.get(1), list.get(2), list.get(3), list.get(4));
+            apiResults.add(apiResult);
+        });
+
+        TestResultSummary testResultSummary = new TestResultSummary(String.valueOf(total), String.valueOf(success), String.valueOf(failed));
+        testResultSummaries.add(testResultSummary);
+        ModelAndView modelAndView = new ModelAndView("/template");
+        modelAndView.addObject("apiResults", apiResults);
+        modelAndView.addObject("testResultSummaries", testResultSummaries);
+        return modelAndView;
+    }
+
+    @RequestMapping("/test111")
     public ModelAndView entitlement() {
         String time = (new Tools()).getTimeName();
         AtomicInteger success = new AtomicInteger();
@@ -44,23 +87,23 @@ public class AccountController {
         accountService.encryptLoginPassword();
         accountService.signIn();
         resultList.clear();
-        apiTestResultService.getByCreateTime(time, "account-user").forEach(e ->
-                resultList.add(Arrays.asList(e.getUrl(), e.getResponseBody(), String.valueOf(e.getStatus_code()), e.getVerification())));
+        apiTestResultService.getByCreateTimeAndSystemName(time, "account-user").forEach(e ->
+                resultList.add(Arrays.asList(e.getUrl(), e.getRemark(), e.getResponseBody() == null ? null: e.getResponseBody().toString(), String.valueOf(e.getStatus_code()), e.getVerification())));
         List<APIResult> apiResults = new ArrayList<>();
         List<TestResultSummary> testResultSummaries = new ArrayList<>();
 
         resultList.forEach(list -> {
-            if (list.get(3).startsWith("Success")) {
+            if (list.get(4).startsWith("Success")) {
                 success.addAndGet(1);
             } else {
                 failed.addAndGet(1);
             }
             total.addAndGet(1);
-            APIResult apiResult = new APIResult(String.valueOf(total.get()), list.get(0), list.get(1), list.get(2), list.get(3));
+            APIResult apiResult = new APIResult(String.valueOf(total.get()), list.get(0), list.get(1), list.get(2), list.get(3), list.get(4));
             apiResults.add(apiResult);
         });
 
-        TestResultSummary testResultSummary = new TestResultSummary(total, success, failed);
+        TestResultSummary testResultSummary = new TestResultSummary(String.valueOf(total), String.valueOf(success), String.valueOf(failed));
         testResultSummaries.add(testResultSummary);
         ModelAndView modelAndView = new ModelAndView("/template");
         modelAndView.addObject("apiResults", apiResults);
