26321793f60a8d8c02dd46a0a2667c811e6b4af0
kevin-jx <kevin-jx@jxinternet.com>
2018-06-01 14:54:55 +0800

    1.编写RestTemplate调用http功能

--- src/main/java/com/apijava/apijava/Utils/RestTem.java
+++ src/main/java/com/apijava/apijava/Utils/RestTem.java
@@ -1,40 +1,56 @@
 package com.apijava.apijava.Utils;
 
 import com.apijava.apijava.domain.ApiInfo;
+import com.apijava.apijava.domain.ApiTestResult;
+import com.apijava.apijava.service.ApiTestResultService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-
+@Component
 public class RestTem {
-    private RestTemplate template;
-    private Tools exchange;
+    private final RestTemplate template = new RestTemplate();
+    private Tools tools;
+    private final ApiTestResultService apiTestResultService;
     private HttpHeaders requestHeaders = new HttpHeaders();
 
     @Autowired
-    public RestTem(RestTemplate template, Tools exchange) {
-        this.template = template;
-        this.exchange = exchange;
+    public RestTem( Tools tools, ApiTestResultService apiTestResultService) {
+        this.tools = tools;
+        this.apiTestResultService = apiTestResultService;
     }
 
-    public String excute(ApiInfo apiInfo) {
-        if (apiInfo.getSetBody().isEmpty()) apiInfo.setSetBody(null);
+    public void excute(ApiInfo apiInfo) {
+        ApiTestResult apiTestResult;
+        apiTestResult = tools.toApiTestResult(apiInfo);
         requestHeaders.clear();
-        requestHeaders = exchange.toHttpHeaders(apiInfo.getHeader());
-        HttpEntity<String> requestEntity = new HttpEntity<>(apiInfo.getSetBody(), requestHeaders);
+        requestHeaders = tools.toHttpHeaders(tools.toJSONObject(apiInfo.getHeader()));
+        HttpEntity<String> requestEntity = new HttpEntity<>(apiInfo.getSetBody() == null ? null: apiInfo.getSetBody().toString(), requestHeaders);
 
         ResponseEntity<String> response;
         try {
-            response = template.exchange(apiInfo.getUrl(), exchange.toHttpMethod(apiInfo.getHttpMethod()), requestEntity, String.class);
+            response = template.exchange(apiInfo.getUrl(), tools.toHttpMethod(apiInfo.getHttpMethod()), requestEntity, String.class);
         } catch (HttpClientErrorException e) {
-            String status = e.getMessage().trim().substring(0, 3);
-            String body = e.getResponseBodyAsString();
-            return status + ",,," + body + ",,," + apiInfo.getUrl();
+            getExceptionResponse(apiInfo, apiTestResult, e);
+            return;
         }
-        return response.getStatusCodeValue() + ",,," + response.getBody() + ",,," + apiInfo.getUrl();
+        tools.responseBodyToApiTestResult(apiTestResult, response);
+        apiTestResultService.insert(apiTestResult);
+    }
+
+    public void getExceptionResponse(ApiInfo apiInfo, ApiTestResult apiTestResult, HttpClientErrorException e) {
+        String status = e.getMessage().trim().substring(0, 3);
+        String body = e.getResponseBodyAsString();
+        apiTestResult.setStatus_code(Integer.parseInt(status));
+        apiTestResult.setResponseBody(body);
+        if (apiInfo.getExpectResult().equals("Failed")) {
+            apiTestResult.setVerification("Success");
+        } else apiTestResult.setVerification("Failed");
+        apiTestResultService.insert(apiTestResult);
     }
 
 }
