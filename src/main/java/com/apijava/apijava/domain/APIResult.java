221d3eafdc396ca47fd6fbf555cee02d4290bec2
kevin-jx <kevin-jx@jxinternet.com>
2018-06-01 14:51:09 +0800

    1.测试结果增加用例名称

--- src/main/java/com/apijava/apijava/domain/APIResult.java
+++ src/main/java/com/apijava/apijava/domain/APIResult.java
@@ -6,13 +6,15 @@ import lombok.Data;
 public class APIResult {
     public String id;
     public String url;
+    public String remark;
     public String responseBody;
     public String status_code;
     public String result;
 
-    public APIResult(String id, String url, String responseBody, String status_code, String result) {
+    public APIResult(String id, String url, String remark, String responseBody, String status_code, String result) {
         this.id = id;
         this.url = url;
+        this.remark = remark;
         this.responseBody = responseBody;
         this.status_code = status_code;
         this.result = result;
