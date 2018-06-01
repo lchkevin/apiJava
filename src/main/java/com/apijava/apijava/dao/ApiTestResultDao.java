ed833f8099180a1b64074c18519f89698cddbda2
kevin-jx <kevin-jx@jxinternet.com>
2018-06-01 10:51:54 +0800

    1.添加数据库查询函数 在查询时间之后的数据 查询接口

--- src/main/java/com/apijava/apijava/dao/ApiTestResultDao.java
+++ src/main/java/com/apijava/apijava/dao/ApiTestResultDao.java
@@ -4,5 +4,9 @@ import com.apijava.apijava.domain.ApiTestResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface ApiTestResultDao extends JpaRepository<ApiTestResult, Long>, JpaSpecificationExecutor<ApiTestResult> {
+    List<ApiTestResult> findAllByCreateTime(String time);
+    List<ApiTestResult> findAllByCreateTimeAfterAndSystemNameEquals(String time, String systemName);
 }
