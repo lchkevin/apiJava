765d424e9674f5b25b3ad157e67a4a1daefa0a5f
kevin-jx <kevin-jx@jxinternet.com>
2018-06-01 18:19:19 +0800

    1.参数化对应的字符串处理

--- src/main/java/com/apijava/apijava/controller/AccountController.java
+++ src/main/java/com/apijava/apijava/controller/AccountController.java
@@ -41,11 +41,15 @@ public class AccountController {
         AtomicInteger failed = new AtomicInteger();
         AtomicInteger total = new AtomicInteger();
         List<List<String>> resultList = new ArrayList<>();
+        String token = "425c735b-788f-4b94-81ad-300f510f8292";
 
         resultList.clear();
         //TODO 从sqlite中获取account的接口进行测试
         String time = (new Tools()).getTimeName();
-        apiInfoService.findAllBySystemName("account-user").forEach(e -> restTem.excute(e));
+        apiInfoService.findAllBySystemName("account-user").forEach(e -> {
+            if (e.getUrl().contains("XXXX")) e.setUrl(e.getUrl().replaceAll("XXXX", token));
+            restTem.excute(e);
+        });
         //TODO 获取测试结果
         apiTestResultService.getByCreateTimeAndSystemName(time, "account-user").forEach(e ->
                 resultList.add(Arrays.asList(e.getUrl(), e.getRemark(), e.getResponseBody() == null ? null: e.getResponseBody().toString(), String.valueOf(e.getStatus_code()), e.getVerification())));
@@ -111,5 +115,21 @@ public class AccountController {
         return modelAndView;
     }
 
+    public static void main(String[] args) {
+        String url = "http://192.168.1.93:8010/api/v1/account/webapi/operate/users/signIn?access_token=XXXX";
+        String token = "MMMM";
+        String body = "{\"qq\":\"258845215\",\"realName\":\"autotest\",\"password\":\"123456\",\"phoneNumber\":\"13996323363\",\"memberType\":\"AGENT\", \"username\":${username}, \"prizeGroup\":\"1950\",\"email\":\"258845215@qq.com\"}";
+        String username = "testName";
+
+        if (url.contains("XXXX")) {
+            url = url.replace("XXXX", token);
+        }
+        log.info(url);
+
+        if (body.contains("${")) {
+            body.replace("${*}", username);
+        }
+        log.info(body);
+    }
 
 }
